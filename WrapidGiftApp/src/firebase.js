31422c4c61243adec2172e2f043d5d36e130c424
dvalerio001 <dvalerio001@gmail.com>
2021-03-05 13:40:24 -0500

    init firebase authentication & LoginScreen styling

---
+++ WrapidGiftApp/src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+// Firebase Configuration
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCORm5vskOyCLjZSpKYsDtGZcKPbJrmicc",
+    authDomain: "wrapid-gift-1693c.firebaseapp.com",
+    projectId: "wrapid-gift-1693c",
+    storageBucket: "wrapid-gift-1693c.appspot.com",
+    messagingSenderId: "492289068346",
+    appId: "1:492289068346:web:13af3e3566aa0062ea9403"
+  };
+  // Initialize Firebase
+  const fire = firebase.initializeApp(firebaseConfig);
+
+  export default fire;
