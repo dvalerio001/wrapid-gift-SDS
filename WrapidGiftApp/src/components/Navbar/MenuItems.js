4e831b9ed25aa277fefd20661bd5cce776cc8cb7
eashl003 <eashl003@plattsburgh.edu>
2021-03-06 20:20:09 -0500

    update

---
+++ WrapidGiftApp/src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+  {
+    title: 'Home',
+    url: '#',
+    cName: 'nav-links'
+  },
+  {
+    title: 'About',
+    url: '#about',
+    cName: 'nav-links'
+  },
+  {
+    title: 'Shop',
+    url: '#shop',
+    cName: 'nav-links'
+  },
+  {
+    title: 'Account',
+    url: '#account',
+    cName: 'nav-links'
+  },
+  {
+    title: 'Sign up',
+    url: '#',
+    cName: 'nav-links-mobile'
+  },
+]
