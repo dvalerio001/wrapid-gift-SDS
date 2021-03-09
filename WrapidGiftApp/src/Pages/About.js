cacd2090f1daddc92ebeca0dbab7c09d80b7e0a4
tylerspags <57504071+tylerspags@users.noreply.github.com>
2021-03-09 14:38:56 -0500

    fix about page and styling

--- WrapidGiftApp/src/Pages/About.js
+++ WrapidGiftApp/src/Pages/About.js
@@ -5,14 +5,14 @@ import './About.css';
 import image from './aboutPage.jpg';
 
 const About = () => {
-  return (
-    <>
-    <Header/>
-    <h1>About</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
 
-    <img src={image} />
-    </>
+  return(
+    <section className = "about-container">
+      <nav>
+        <Header />
+      </nav>
+      <h1>About</h1>
+  </section>
   );
 };
 
