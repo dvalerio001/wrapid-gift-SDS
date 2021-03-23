d880dca05a42bf464de56f7cdea7a0b8d10b516f
tylerspags <57504071+tylerspags@users.noreply.github.com>
2021-03-23 17:10:03 -0400

    changed colors to match, added new contact page

--- WrapidGiftApp/src/Pages/Contact.js
+++ WrapidGiftApp/src/Pages/Contact.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from "../components/Header";
 import './Contact.css';
+import Phone from './phoneIcon.png'
+import Email from './email.png'
+import {database} from '../firebase'
 
 const Contact = () => {
 
     return(
-        <section className = "Contact-container">
+        <section className="whole-contact">
             <nav>
-              <Header />
+                <Header />
             </nav>
-            <h1>Contact Us</h1>
+            <section className="app">
+                <form className="form">
+                    <h1>Contact Form</h1>
+
+                    <label>Name</label>
+                    <input placeholder="Name" />
+
+                    <label>Email</label>
+                    <input placeholder="Email" />
+
+                    <label>Message</label>
+                    <textarea placeholder="Message" ></textarea>
+
+                    <button type="submit">Submit</button>
+                </form>
+            </section>
         </section>
     );
 };
